39dd51e06402f502afc977364ed89a2d64144786
vic12141983 <60449014+vic12141983@users.noreply.github.com>
2024-02-11 12:08:16 -0700

    update on initial login page

--- myproject/main.py
+++ myproject/main.py
@@ -1,2 +1,10 @@
-from flask import Flask
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("login_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
