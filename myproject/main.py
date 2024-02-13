8be881425279593eb50886610777d0e24abfc297
leilaale <leilaalemtz.11@gmail.com>
2024-02-12 19:56:26 -0700

    error fixed, home page accessible

--- myproject/main.py
+++ myproject/main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+def login():
+    return render_template("login_page.html")
+
+@app.route("/main_page")
 def main_page():
     return render_template("main_page.html")
 
