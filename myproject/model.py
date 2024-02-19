5ecfc99570201871a70ab998d74c1de511c56e8e
a-s-o-m <s@Chromebook.local>
2024-02-19 15:26:45 -0700

    Add functionality to upload a pdf

--- myproject/model.py
+++ myproject/model.py
@@ -1,18 +1,18 @@
 from PyPDF2 import PdfReader
-from pymongo import MongoClient
 import os
-from urllib.parse import quote_plus
 
-client = MongoClient('mongodb+srv://angel:angel123@cluster0.hi3uuvt.mongodb.net/?retryWrites=true&w=majority')
-db = client['proposal_db']
-collection = db['proposal_collection']
+'''
+TODO: Add description, handle exceptions
+'''
+def get_pdf_data(pdf):
+    if not pdf: return
 
-with open('static/files/example.pdf', 'rb') as file:
-    pdf_data = file.read()
+    with open(pdf, 'rb') as file:
+        pdf_data = file.read()
 
-inserted_id = collection.insert_one({'pdf': pdf_data}).inserted_id
-print(f"PDF uploaded to MongoDB with ID: {inserted_id}")
+    return pdf_data
+    
 
-reader = PdfReader("static/files/example.pdf")
-page = reader.pages[2]
-print(page.extract_text())
+# reader = PdfReader("static/files/example.pdf")
+# page = reader.pages[2]
+# print(page.extract_text())
