749eddf3cd545f0fe402686739d1d93a9596c1d2
a-s-o-m <s@Chromebook.local>
2024-02-19 20:00:04 -0700

    Add extendable template for admin pages

--- myproject/main.py
+++ myproject/main.py
@@ -21,6 +21,10 @@ def main_page():
     
     return render_template("main_page.html")
 
+@app.route("/user_management")
+def user_management():
+     return render_template("admin_usr_mgmt.html")
+     
 @app.route("/test_page")
 def test_page():
         return render_template("test_page.html")
