7f7a20e3e94896f16a420f8e0d617970f3dfc671
Victor-HerreraGit <vherreravi@miners.utep.edu>
2024-03-04 23:58:37 -0700

    Added user and admin roles

--- myproject/main.py
+++ myproject/main.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for,flash
+import secrets
 from pymongo import MongoClient
 
 app = Flask(__name__)
+#generate key everytime since users will be stored locally 
+app.secret_key = secrets.token_hex(16)
+
 client = MongoClient('mongodb+srv://angel:angel123@cluster0.hi3uuvt.mongodb.net/?retryWrites=true&w=majority')
 db = client['test_db']
 proposals_collection = db['proposals']
 
-@app.route("/")
-def login():
+
+
+
+
+users = {
+    'adminUser': {'password': 'adminPass', 'role': 'admin'},
+    'regularUser': {'password': 'userPass', 'role': 'user'}
+}
+
+@app.route("/", methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Directly check against a known username and password for debugging
+        if username == 'testUser' and password == 'testPass':
+            flash("Login successful!")
+            return redirect(url_for('main_page'))  
+        else:
+            flash("Invalid username or password")
+        
     return render_template("login_page.html")
+
+
 
 @app.route("/main_page")
 def main_page():
